cb72b73315317c84eca152acdf7bcb171a486233
Sam Hill <samuhill@gmail.com>
2014-05-16 16:07:37 -0400

    alternate fullscreen code

--- assets/js/main.js
+++ assets/js/main.js
@@ -52,7 +52,20 @@ function getComments() {
 }
 
 function ToggleFullScreen () {
-   document.body.requestFullScreen();
-   document.body.webkitRequestFullScreen();
-   document.body.mozFullScreen();
+   var el = document.documentElement
+	, rfs = // for newer Webkit and Firefox
+	       el.requestFullScreen
+	    || el.webkitRequestFullScreen
+	    || el.mozRequestFullScreen
+	    || el.msRequestFullScreen
+    ;
+    if(typeof rfs!="undefined" && rfs){
+      rfs.call(el);
+    } else if(typeof window.ActiveXObject!="undefined"){
+      // for Internet Explorer
+      var wscript = new ActiveXObject("WScript.Shell");
+      if (wscript!=null) {
+	 wscript.SendKeys("{F11}");
+      }
+    }
 }
