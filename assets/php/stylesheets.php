b7e9105241b3095fc2aa75b557ba4cf933ab1574
Sam Hill <samuhill@gmail.com>
2014-05-16 16:15:30 -0400

    enable debug statements

--- assets/php/stylesheets.php
+++ assets/php/stylesheets.php
@@ -1,4 +1,6 @@
 <?php
+    error_reporting(E_ALL);
+    ini_set('display_errors', True);
     echo '<link rel="stylesheet" type="text/css" href="assets/libraries/bootstrap/css/bootstrap.min.css">
     <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="assets/css/main.css">';
